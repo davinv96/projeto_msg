5870ddedc3f3912edb49d358f24459711ad41a40
davinv96 <davinv96@outlook.com>
2018-04-22 21:00:26 -0300

    commit

--- enviar_mensagem.php
+++ enviar_mensagem.php
@@ -7,11 +7,12 @@
         $id_conversa = mysqli_real_escape_string($link, $_POST['id_conversa']);
         $usuario_envio = mysqli_real_escape_string($link, $_POST['usuario_envio']);
         $usuario_destino = mysqli_real_escape_string($link, $_POST['usuario_destino']);
+
         $id_conversa = base64_decode($id_conversa);
         $usuario_envio = base64_decode($usuario_envio);
         $usuario_destino = base64_decode($usuario_destino);
         
-        $q = mysqli_query($link, "INSERT INTO `messages` VALUES ('','$id_conversa','$usuario_envio','$usuario_destino','$message')");
+        $q = mysqli_query($link, "INSERT INTO mensagens VALUES ('','$id_conversa','$usuario_envio','$usuario_destino','$message')");
         if($q){
             echo "Posted";
         }else{
--- obter_mensagens.php
+++ obter_mensagens.php
@@ -3,14 +3,14 @@
     $link = conecta();
 
     if(isset($_GET['c_id'])){
-        $conversation_id = base64_decode($_GET['c_id']);
-        $query = mysqli_query($link, "SELECT * FROM `messages` WHERE conversation_id='$conversation_id'");
+        $id_conversa = base64_decode($_GET['c_id']);
+        $query = mysqli_query($link, "SELECT * FROM conversas WHERE id_conversa='$id_conversa'");
         if(mysqli_num_rows($query) > 0){
             while ($m = mysqli_fetch_assoc($query)) {
                 $usuario_envio = $m['usuario_envio'];
                 $usuario_destino = $m['usuario_destino'];
                 $message = $m['message'];
-                $user = mysqli_query($link, "SELECT nome FROM `usuario` WHERE id='$usuario_envio'");
+                $user = mysqli_query($link, "SELECT nome FROM usuario WHERE id_usuario='$usuario_envio'");
                 $user_fetch = mysqli_fetch_assoc($user);
                 $user_form_username = $user_fetch['nome'];
                 echo "
