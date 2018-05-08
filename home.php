35e2284cfbb82cc6f71553e8f03db0bfc4914965
davinv96 <davinv96@outlook.com>
2018-05-07 21:34:05 -0300

    commit

--- home.php
+++ home.php
@@ -17,6 +17,12 @@
         $nome = $row['nome'];
     }
     
+    $q2= mysqli_query($con, "SELECT `usuario_destino` FROM mensagens WHERE `lida` < NOW() - INTERVAL 30 MINUTE and `usuario_envio` =' $id_login'");
+    while($row = mysqli_fetch_assoc($q2)){
+        $usuario_dest = $row['usuario_destino'];
+    }
+
+
     
 ?>
 <!DOCTYPE html>
@@ -44,7 +50,31 @@
                     <a class="navbar-brand" href="http://localhost/projeto_msg/home.php">UlbraChat - Olá, <?php echo $nome; ?></a>
                    
                 </div>
+            <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
 
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
