d55958809c8df9114a622a8b7c82184108d28799
davinv96 <davinv96@outlook.com>
2018-08-16 18:09:39 -0300

    commit

--- admin/example_001.php
+++ admin/example_001.php
@@ -10,13 +10,13 @@ $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8',
 
 // set document information
 $pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Nicola Asuni');
-$pdf->SetTitle('TCPDF Example 001');
-$pdf->SetSubject('TCPDF Tutorial');
-$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+$pdf->SetAuthor('ULBRA - Campus Guaíba');
+$pdf->SetTitle('Relatório de conversas');
+$pdf->SetSubject('Relatório de conversas');
+
 
 // set default header data
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING, array(0,64,255), array(0,64,128));
 $pdf->setFooterData(array(0,64,0), array(0,64,128));
 
 // set header and footer fonts
@@ -65,7 +65,7 @@ if(isset($_GET['id'])){
 
 	// Set some content to print
 	$head = "<table border='1'>
-	<center><h4>Mensagens enviadas por ".$nome_remet."</h4></center>
+	<center><h4>Mensagem enviada pelo usuário ".$nome_remet."</h4></center>
 	<br>
 
 	<br>
@@ -79,7 +79,7 @@ if(isset($_GET['id'])){
 				<th> Visualizada pelo destinatário</th>
 							
 			</tr>";
-
+	//$pdf->writeHTML($head, 0, 1, 0, true, '', true);	
 	$aux = 0;
 
 	$q3 = mysqli_query($con, "SELECT `usuario_destino`,`mensagens`,`timestamp`,`lida` FROM `mensagens` 
@@ -109,14 +109,13 @@ if(isset($_GET['id'])){
 				$end = "</tr>
 						</table>";
 
-			$pdf->writeHTMLCell(0, 0, '', '', $head.$body.$body2.$end, 0, 1, 0, true, '', true);		
+			$pdf->writeHTML($head.$body.$body2.$end,  0, 1, 0, true, '', true);		
 				
 		}
 			
 }
 // Print text using writeHTMLCell()
 
-	$pdf->writeHTMLCell(0, 0, '', '', $head.$body.$body2.$end, 0, 1, 0, true, '', true);
 
 
 
